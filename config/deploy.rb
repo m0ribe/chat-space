a660c31511541d780968025f049f07dbee66911b
m0ribe <moro.recr@gmail.com>
2020-06-17 19:54:03 +0900

    FIx 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "chat-space""
-set :repo_url, "git@github.com:m0ribe/chat-space.git"
+set :application, "chat-space"
+set :repo_url,  'git@github.com:m0ribe/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
